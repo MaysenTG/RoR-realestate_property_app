7e35d79b55f1b72bb5d58890018da182dd71b311
Maysen Greenwood <greenwood.maysen@gmail.com>
2022-04-26 20:32:45 +1200

    Update views for better styling/formatting

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,10 +4,11 @@ class PublicController < ApplicationController
   end
 
   def latest
-    @properties = Property.order(created_at: :desc).limit(5)
+    @latest_properties = Property.order(created_at: :desc).limit(5)
   end
 
   def featured
+    @featured_properties = Property.order(price: :desc).limit(5)
   end
 
   def sale
